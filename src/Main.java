743957c6dd10714a3654aa4f5362e0a9166133ef
Anton Kytmanov <antonkytm@mail.ru>
2023-02-04 17:41:38 +0500

    добавлен README и демонстрация работы класса SalesManager в Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] salesSergey = {5,25,5,8,4,6,3,5,1,1515,81,444,77856,111,44};
+        int [] salesSveta = {5,20,2,5,5,33,65,85,0,-455,2,3,5,88888888,5};
+
+        SalesManager salesManagerSergey = new SalesManager(salesSergey);
+        SalesManager salesManagerSveta = new SalesManager(salesSveta);
+
+        System.out.println(salesManagerSergey.max());
+        System.out.println(salesManagerSveta.max());
     }
 }
